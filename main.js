14e6a080d79f66624e69f4134212177333161cd4
kerolos Magdy <kero.magdy2000@gmail.com>
2024-10-05 23:53:31 +0300

    save

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/solutions/typescript
 var stringToArray = (s: string): string[] => s.split(" ");
 console.log(stringToArray("hello world"));
 */
+
+// ignoreNumbers = (str) =>
+//   str
+//     .split("")
+//     .map((c) => c.replace(/[0-9]/g, ""))
+//     .join("");
+
+ignoreNumbers = (str) =>
+  str
+    .split("")
+    .filter((c) => !Number(c))
+    .join("");
+
+console.log(ignoreNumbers("hello123world"));
