f583e73057752e1be5c6475a0b39a3bda112b63c
kerolos Magdy <kero.magdy2000@gmail.com>
2024-11-12 18:57:35 +0200

    save

--- index.js
+++ index.js
@@ -554,3 +554,28 @@ countBits = (n) =>
 
 console.log(countBits(40));
 */
+
+/*
+7 kyu Bubblesort Once
+https://www.codewars.com/kata/56b97b776ffcea598a0006f2/train/javascript
+bubblesortOnce = (a) =>
+  a.reduce((a, c, i, arr) => {
+    if (c > arr[i + 1]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    }
+    return a;
+  }, a);
+
+  but this is accepted solve
+  function bubblesortOnce(a) {
+  const newArr = [...a];
+  return newArr.reduce((acc, curr, i) => {
+    if (newArr[i] > newArr[i + 1]) {
+      [newArr[i], newArr[i + 1]] = [newArr[i + 1], newArr[i]];
+    }
+    return newArr;
+  }, newArr);
+}
+
+console.log(bubblesortOnce([9, 7, 5, 3, 1, 2, 4, 6, 8]));
+*/
