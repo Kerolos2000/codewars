4a0bf7a960e31210350b372f27bd73339a1618a1
Kerolos2000 <kero.magdy2000@gmail.com>
2025-12-25 23:51:17 +0200

    solve 8 kyu Sentence Smash

--- index.js
+++ index.js
@@ -1106,3 +1106,10 @@ function onlyOne(...flags) {
     return flags.filter(Boolean).length === 1;
 }
 */
+
+/*
+8 kyu Sentence Smash
+https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+smash = (w) => w.join(" ");
+*/
